923ea2094e278fde655add41ea1df55c8e3029b0
Jann-Niklas Zimmermann <jann.n.zimmermann+github@gmail.com>
2023-01-12 23:29:58 +0100

    Add test to check missing file in upload

--- tests/unit/import.test.js
+++ tests/unit/import.test.js
@@ -4,14 +4,12 @@
 
 const imp = require('../../js/import.js')
 
-describe('Testing "displayQrContent"', () => {
+describe('Testing "displayQrContent()"', () => {
 
     beforeEach(() => {
         // Mock HTML document for function call
         document.body.innerHTML =
-            '<div id="qr-content">' +
-            ''+
-            '</div>';
+            '<div id="qr-content"></div>';
     })
 
     test('Test display valid JSON', () => {
@@ -25,4 +23,17 @@ describe('Testing "displayQrContent"', () => {
         expect(document.querySelector('#qr-content label').innerHTML)
             .toBe("name: test");
     });
-})
+});
+
+describe('Testing "decodeFile()"', () => {
+
+    beforeEach(() => {
+        document.body.innerHTML =
+            '<input name="datei" id="uploadInput" type="file" size="50" accept="image/*">';
+        const uploadInput = document.getElementById("uploadInput");
+    })
+
+    test('Test empty upload', () => {
+        expect(() => imp.getFile(uploadInput, null)).toThrow(/^No file selected!$/);
+    })
+});
