901c808b5fa897827f75054a3a85061774a0073b
Jann-Niklas Zimmermann <jann.n.zimmermann+github@gmail.com>
2023-01-21 15:06:52 +0100

    Add unit tests (#5)
    * Update workflow
    
    * Add node.js support
    
    * Import JQuery in html files
    
    * Add sample tests
    
    * Move jsqrcode_lazarsoft to different folder
    
    * refactor file for qr code creation
    
    * Update html forms
    
    * Add first test for import.js
    
    * Use describe block for better structure
    
    * remove console logs
    
    * refactor decoding function (move parts outside of function)
    
    * Add test to check missing file in upload
    
    * Drei neue Tests angefangen, aber noch nicht fertig gestellt.
    Syntax enthaelt noch Fehler
    - Test without JSON Object
    - Test with NULL
    - Test with two different Values
    
    * - Test without JSON Object (Fehler behoben)
    - Test with NULL  (Fehler behoben)
    - Test with two different Values (läuft noch nicht)
    
    * Tests bearbeitet und import.js bearbeitet, displayQRContent erweitert
    
    * Tests laufen alle korrekt durch.
    
    * Add test for more key-value pairs in json
    
    * Add additional check for non-presence of previous result
    
    * clean up
    
    * update order of ci jobs
    
    Co-authored-by: MrTobiasWagner <s79080@bht-berlin.de>

---
+++ js/createQr.js
@@ -0,0 +1,48 @@
+let formData;
+
+function createQrFromForm(formID) {
+    saveFormData(formID)
+    generateQRCode(formData);
+    enableDownload();
+}
+
+// Set data from the form fields
+function saveFormData(formID) {
+    let form = document.getElementById(formID);
+    formData = new FormData(form);
+}
+
+function getFormData() {
+    return formData;
+}
+
+// Generates the QR-Code and displays it
+function generateQRCode(formData) {
+    // Parse JSON from FormData
+    let obj = {};
+    formData.forEach((value, key) => obj[key] = value);
+    let json = JSON.stringify(obj);
+
+    // Clear existing QR code
+    let qrDiv = document.getElementById('qrImage');
+    while (qrDiv.firstChild) {
+        qrDiv.removeChild(qrDiv.firstChild);
+    }
+
+    // Create new QR code
+    new QRCode(qrDiv, json);
+}
+
+// Set hrefs for download
+function enableDownload() {
+    // https://stackoverflow.com/a/12796748
+    // Parse href from image
+    let canvas = document.querySelector("#qrImage canvas")
+    let downloadHref = canvas.toDataURL('image/png');
+    downloadHref = downloadHref.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+    // Set href for download button
+    document.querySelector("#qrImageDownload").setAttribute("href", downloadHref);
+    let downloadBtn = document.querySelector("#qrDownloadButton");
+    downloadBtn.setAttribute("href", downloadHref);
+}
